c685089efcb46865330cb9d5f96c5beb12f69c4e
mtd8899 <michaeldenum89@gmail.com>
2023-10-31 09:31:44 -0400

    add error handling

--- server.py
+++ server.py
@@ -17,11 +17,13 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
     text_to_analyze = request.args.get('textToAnalyze')
     response = sentiment_analyzer(text_to_analyze)
     label = response['label']
     score = response['score']
+
+    if label is None:
+        return "Invalid input ! Try again."
     return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 @app.route("/")
@@ -29,10 +31,9 @@ def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
     return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
     app.run(host="0.0.0.0", port=5000)
